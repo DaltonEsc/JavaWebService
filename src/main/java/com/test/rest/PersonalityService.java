e47dfd5c8500a16383c2f40640c8f81b406e554e
Dalton Eschberger <eschberger.dalton@gmail.com>
2020-04-30 14:53:45 -0500

    bug fix

--- src/main/java/com/test/rest/PersonalityService.java
+++ src/main/java/com/test/rest/PersonalityService.java
@@ -9,7 +9,7 @@ public class PersonalityService {
     @GET // This annotation indicates GET request
     @Path("/Personality")
     public Response Personality() {
-        long rand = Math.round(Math.random());
+        String rand = String.valueOf(Math.round(Math.random()));
         return Response
                 .status(Response.Status.OK)
                 .entity(rand)
